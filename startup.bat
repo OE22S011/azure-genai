f261d44ad97d086a6ac9edc026458f3a8284d81a
OE22S011 <sudha.adabala620@gmail.com>
2026-03-16 12:56:52 +0530

    Day 3: Built RAG pipeline from sratch

---
+++ startup.bat
@@ -0,0 +1,5 @@
+set AZURE_OPENAI_KEY=D3xOp7O5HwEzItEh2Ih49yNpca0Quf7eI7pVtCXZBTz2uID9Z6sEJQQJ99CCACYeBjFXJ3w3AAAAACOGobuC
+set AZURE_OPENAI_ENDPOINT=https://sudha-mmkhx2sq-eastus2.cognitiveservices.azure.com/openai/deployments/gpt-4o-mini/chat/completions?api-version=2025-01-01-preview
+set AZURE_SAS_URL="https://genaistoragesudha.blob.core.windows.net/mydata/daily-minimum-temperatures-in-me.csv?sp=r&st=2026-03-13T05:33:30Z&se=2026-03-20T13:48:30Z&spr=https&sv=2024-11-04&sr=b&sig=%2F7JNFgGoXCW3mZ0kKv6gG6Qu8BI0VGww0ovi2ZBe%2FOw%3D"
+cd C:\Users\ADABASS\Desktop\Sudha\azure_practice
+jupyter notebook
